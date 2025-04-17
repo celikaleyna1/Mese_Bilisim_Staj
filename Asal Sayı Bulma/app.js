1fdf7b397f8447ffcfa7561cb12dcccda2cc2324
celikaleyna1 <ale2004cell@gmail.com>
2025-04-18 00:25:43 +0300

    Add files via upload

---
+++ Asal Sayı Bulma/app.js
@@ -0,0 +1,16 @@
+let sayi = Number (prompt("Lütfen bir sayı giriniz."));
+let sonuc = true;
+for(let i=2 ; i<=Math.floor(sayi/2); i++){
+    if(sayi%i==0){
+        sonuc = false;
+        break;
+    }
+}
+
+if(sonuc){
+    alert(sayi + " asaldır.");
+
+}
+else{
+    alert(sayi + " asal değildir. ");
+}
