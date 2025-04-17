3800ad9541e640aa856face27dc2128f490bec0f
celikaleyna1 <ale2004cell@gmail.com>
2025-04-18 00:44:00 +0300

    Add files via upload

---
+++ 10'luk Sayıyı 2'liğe Çevirme/app.js
@@ -0,0 +1,9 @@
+let sayi = Number(prompt("Dönüştürmek istediğiniz sayıyı giriniz."));
+
+function donustur (sayi){
+let binary = "";
+while(true){
+    binary=(sayi%2).toString();
+    sayi=Math.floor(sayi/2);
+} 
+}
