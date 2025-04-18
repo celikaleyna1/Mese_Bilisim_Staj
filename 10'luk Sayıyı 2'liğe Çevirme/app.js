c26994cb37f68c5692535294f60fc9ac4a52597b
celikaleyna1 <ale2004cell@gmail.com>
2025-04-18 15:36:49 +0300

    Delete 10'luk Sayıyı 2'liğe Çevirme directory

--- 10'luk Sayıyı 2'liğe Çevirme/app.js
+++
@@ -1,9 +0,0 @@
-let sayi = Number(prompt("Dönüştürmek istediğiniz sayıyı giriniz."));
-
-function donustur (sayi){
-let binary = "";
-while(true){
-    binary=(sayi%2).toString();
-    sayi=Math.floor(sayi/2);
-} 
-}
